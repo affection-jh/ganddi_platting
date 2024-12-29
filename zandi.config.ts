2d28db1d1044a32d534a56085363eb86fe25c003
jaehun <jaehun330@gmail.com>
2024-12-30 04:03:19 +0900

    2025-01-01

--- zandi.config.ts
+++ zandi.config.ts
@@ -7,7 +7,7 @@ const config: ConfigDto = {
     subtitle: `${new Date().getFullYear()} EOS 겨울방학`,
     title: "아찔한 잔디심기",
     icon: "plant.png",
-    globalStartDate: "2024-12-30",
+    globalStartDate: "2025-12-30",
     globalDueDate: "2025-01-31",
     users: [
         {
