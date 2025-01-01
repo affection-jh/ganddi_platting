e8cb0bee88ca4558ad29ddf6b7d526e52efd5817
jaehun <jaehun330@gmail.com>
2025-01-01 23:30:10 +0900

    correct fine problem

--- src/util/defaultFineFormula.ts
+++ src/util/defaultFineFormula.ts
@@ -3,7 +3,13 @@ import {Contribution} from "../interface/contribution";
 
 /// 기본 벌금 계산 공식
 /// 연속된 날 동안 커밋을 하지 않았다면, 그 전날 벌금의 2배 만큼 총 벌금에 더해진다. 단, 하루 벌금은 최대 8000원을 넘을 수 없다.
-const defaultFineFormula: FineFormula = (contributions: Contribution[]) => {
+const defaultFineFormula: FineFormula = (contributions: { date: Date; count: number; timeout: boolean; }[] = [
+    { date: new Date("2024-01-01"), count: 0, timeout: false },
+    { date: new Date("2024-01-02"), count: 0, timeout: false },
+    { date: new Date("2024-01-03"), count: 0, timeout: false },
+    { date: new Date("2024-01-04"), count: 0, timeout: false },
+    { date: new Date("2024-01-05"), count: 0, timeout: false },
+]) => {
     let fine = 0;
     let consecutiveZeroDays = 0;
 
@@ -15,9 +21,10 @@ const defaultFineFormula: FineFormula = (contributions: Contribution[]) => {
         } else {
             consecutiveZeroDays = 0;
         }
+        
 
         if (consecutiveZeroDays > 0) {
-            let temp = Math.pow(2, consecutiveZeroDays - 1) * 500
+            let temp = Math.pow(2, consecutiveZeroDays-1) * 500
             fine += temp > 8000 ? 8000 : temp;
         }
     }
--- zandi.config.ts
+++ zandi.config.ts
@@ -1,36 +1,38 @@
 import {ConfigDto} from "./src/interface/dto/config.dto";
 import {ConfigConverter} from "./src/util/converter/config.converter";
-import { formatDate } from "./src/util/date";
 import defaultFineFormula from "./src/util/defaultFineFormula";
 
 const config: ConfigDto = {
     subtitle: `${new Date().getFullYear()} EOS 겨울방학`,
     title: "아찔한 잔디심기",
     icon: "plant.png",
-    globalStartDate: "2025-01-02",
-    globalDueDate: "2025-02-28",
+    globalStartDate: "2025-01-01",
+    globalDueDate: null,
     users: [
         {
-            name: "정재훈",
-            id: "affection-jh",
-            startDate: "2025-01-01",
-            paid: 0,
-            timeoff: [],
-        },
-        {
-            name: "노영진",
-            id: "yyoungjin",
-            startDate: "2025-01-01",
-            paid: 0,
-            timeoff:[],
-        },
-        {
-            name: "신영빈",
-            id: "youngbin03",
-            startDate: "2025-01-01",
-            paid: 0,
-            timeoff:[],
+            name : '정재훈',
+            id: 'affection-jh',
+            startDate: '2025-01-01',
+            paid : 0,
+            timeoff : ['2024-12-25'],
+            
         }
+        ,{
+            name : '노영진',
+            id: 'yyoungjin',
+            startDate: '2025-01-01',
+            paid:0,
+            timeoff:['2024-12-25']
+        }
+        ,{
+           
+            name : '신영빈',
+            id: 'youngbi',
+            startDate: '2025-01-01',
+            paid:0,
+            timeoff:['2024-12-25'] 
+        }
+       
     ],
     fineFormula: defaultFineFormula,
 }
